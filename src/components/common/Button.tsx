ad9673a224878604b4893714ff5580e93f28dd81
ibeenoch <fredenoch1@gmail.com>
2025-03-19 20:32:51 +0100

    old setup

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -3,7 +3,7 @@ import React from "react";
 // reuseable components
 interface ButtonProps {
   text: string;
-  onClick: () => void;
+  onClick?: () => void;
   className?: string;
   type?: "button" | "submit" | "reset" | undefined;
 }
@@ -18,7 +18,7 @@ const CustomButton: React.FC<ButtonProps> = ({
     <button
       type={type ? type : "button"}
       onClick={onClick}
-      className={` ${className}`}
+      className={`bg-black text-white text-xs font-md px-8 py-4 rounded  ${className}`}
     >
       {text}
     </button>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,22 +1,26 @@
+import CustomButton from "../common/Button";
+
 const Header = () => {
+  const handleLogin = () => {};
+  const handleRegister = () => {};
   return (
-    <header className="bg-whitebg w-full flex p-6 items-center">
+    <header className="bg-whitebg w-full ">
       <nav className="flex w-full items-center justify-between">
         <div>
           <h1 className="text-xl font-extrabold">ChiMark Pharmacy</h1>
         </div>
 
         <ul className="flex items-center gap-4">
-          <li>Book</li>
+          <li className="bg-black p-8 rounded-md text-white text-xs">Book</li>
           <li>Manage</li>
           <li>Experience</li>
           <li>where </li>
           <li>Book</li>
         </ul>
 
-        <div>
-          <button type="button">Login</button>
-          <button type="button">Register</button>
+        <div className="flex gap-4">
+          <CustomButton text="Login" onClick={handleLogin} />
+          <CustomButton text="Register" onClick={handleRegister} />
         </div>
       </nav>
     </header>
