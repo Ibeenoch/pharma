d38dfb1330668b118c752738c04d8bc513c56979
ibewunjo enoch <fredenoch1@gmail.com>
2025-04-11 18:38:11 +0100

    adding update product

--- src/features/product/productSlice.ts
+++ src/features/product/productSlice.ts
@@ -1,10 +1,10 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../../redux/store";
-import { productDataProps } from "../../types/product/ProductData";
+import { ProductDataProps } from "../../types/product/ProductData";
 
 interface productState {
   status: "idle" | "loading" | "success" | "failure";
-  product: productDataProps;
+  product: ProductDataProps;
 }
 
 const initialState: productState = {
@@ -15,12 +15,12 @@ const initialState: productState = {
     description: "",
     brand: "",
     category: "",
-    qty: 0,
+    quantity: 0,
     additionalInfo: "",
     discount: 0,
-    expiration: "",
+    expirationDate: "",
     isHotDeal: false,
-    serialNo: "",
+    productSerialNo: "",
     creator: '',
     imagesUrl: []
   },
