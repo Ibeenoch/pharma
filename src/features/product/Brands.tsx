162da63d74f958b0dc5104810c68c53b28ce08f4
ibewunjo enoch <fredenoch1@gmail.com>
2025-05-06 16:03:47 +0100

    change grid layout

--- src/features/product/Brands.tsx
+++ src/features/product/Brands.tsx
@@ -1,12 +1,5 @@
 import CustomText from "../../components/common/Text";
 import TwoTextSpan from "../../components/home/TwoTextSpan";
-import b1 from "../../assets/images/b1_mopson.png";
-import b2 from "../../assets/images/b2_emzor.png";
-import b3 from "../../assets/images/b3_swipha.png";
-import b4 from "../../assets/images/b4_mayandbaker.png";
-import b5 from "../../assets/images/bg7-fidson-logo.png";
-import b6 from "../../assets/images/bg5_chemiron.png";
-import b7 from "../../assets/images/b6_glaxos-logo.png";
 import { productBrands } from "../../utils/admin/product/productList";
 import { NavigateFunction } from "react-router-dom";
 import React from "react";
@@ -25,7 +18,7 @@ const Brands:React.FC<BrandsProps> = ({ navigate }) => {
     <section className="border-b border-black mx-4 sm:mx-0 mb-2 pb-4 animate-on-scroll">
       <TwoTextSpan leftText="Top Brands" />
 
-      <article className="flex items-center lg:grid lg:grid-cols-7 gap-4 overflow-x-auto">
+      <article className="grid grid-cols-2 lg:grid-cols-7 gap-4 overflow-x-auto">
         {productBrands.slice(1).map((item, index) => (
           <div onClick={() => handleNavBrand(item.label)} className="cursor-pointer" key={index}>
             <div className="h-46 w-40 xl:h-48 xl:w-42 flex items-center justify-center rounded-xl p-2 bg-white mb-4">
