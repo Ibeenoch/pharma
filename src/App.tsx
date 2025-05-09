bc82d88139b774f1cbe60a63feb7f962ef2316a6
ibewunjo enoch <fredenoch1@gmail.com>
2025-05-09 21:24:29 +0100

    added search for admin and search result

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Suspense } from "react";
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { lazy, Suspense } from "react";
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
-import CursorColor from "./components/common/CursorColor";
-import PageLoadingDisplay from "./components/common/PageLoadingDisplay";
+// import CursorColor from "./components/common/CursorColor";
+const CursorColor =  lazy(() => import("./components/common/CursorColor"));
+const PageLoadingDisplay =  lazy(() => import("./components/common/PageLoadingDisplay"));
+// import PageLoadingDisplay from "./components/common/PageLoadingDisplay";
 
 function App() {
   return (
