0ad17a2afce13598f0f860351140f5b5d4e28961
ibewunjo enoch <fredenoch1@gmail.com>
2025-05-14 13:09:13 +0100

    added product dispatch in useefect

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -43,7 +43,7 @@ const Home = () => {
       // when the user visit the page move the page to the top
       window.scrollTo(0,0);
       // set the correct navbar active text
-      fetchAllProductWithoutPagination()
+      dispatch(fetchAllProductWithoutPagination());
       dispatch(setNavIndexLink({ name: links[0].name, index: 0 }));
     },[])
 
