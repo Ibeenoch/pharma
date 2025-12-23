833950468cecde7d029b1d0a6f126fbc921f1312
Mr Api <94024099+Ibeenoch@users.noreply.github.com>
2025-12-23 13:26:46 +0100

    cursor moving

--- src/components/common/CursorColor.tsx
+++ src/components/common/CursorColor.tsx
@@ -14,12 +14,15 @@ const CursorColor = () => {
 
   return (
     <div className="fixed top-0 left-0 pointer-events-none w-full h-full">
-      <div
+      <div className="p-2 border-2 border-amber-500/30 rounded-full">
+         <div
         className="absolute w-3 h-3 bg-amber-500/30 rounded-full transition-transform duration-50"
         style={{
           transform: `translate(${position.x - 8}px, ${position.y - 8}px)`,
         }}
       />
+      </div>
+   
     </div>
   );
 };
